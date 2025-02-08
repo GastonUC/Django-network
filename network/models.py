264e96a10bbf862dfd388f90e1c018214b4f9486
GastonUC <Gacox.magallanes@gmail.com>
2025-02-08 00:01:29 -0300

    Added models for the database

--- network/models.py
+++ network/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="likes", default=0)
+
+    def __str__(self):
+        return f"{self.user}: {self.content[:50]}"
+
+class Follower(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+
+    def __str__(self):
+        return f"{self.user} follows {self.follower}"
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_likes")
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
